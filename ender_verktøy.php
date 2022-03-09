1879585e74cfbb86b8046c2cab52a22d4cbab070
stig <92983848+stigamyr@users.noreply.github.com>
2022-03-09 15:34:46 +0100

    jeg har kommet i gang med koden for og opdaterre verktøy
    jeg har kommet i gang med koden for og opdaterre verktøy

---
+++ ender_verktøy.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=ph, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
+// Prepare statement
+$stmt = $conn->prepare($sql);
+
+// execute the query
+$stmt->execute();
+
+// melding om at vrekyet er opdatert
+echo $stmt->rowCount() . " dur har opdatert verktøyene i data basen";
+} catch(PDOException $e) {
+echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+
+?>
+
+
+
+</body>
+
+
+
+</html>
+
