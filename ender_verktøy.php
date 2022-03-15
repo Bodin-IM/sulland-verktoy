c3f60173ed69d3a5e9b4632b9a4de3ab108617e8
stig <92983848+stigamyr@users.noreply.github.com>
2022-03-15 14:51:40 +0100

    Update ender_verktøy.php

--- ender_verktøy.php
+++ ender_verktøy.php
@@ -1,38 +1,118 @@
+
+
+<?php
+include "meny.php";
+
+if(isset($_POST["endre_id"])){
+  $servername = "10.100.0.2";
+  $username = "im2a";
+  $password = "Passord2";
+  $dbname = "sulland_verktoy";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+
+  
+  
+}  else{
+  die("du må vlge et vrektøy");
+}
+
+
+$sql = "SELECT * FROM verktoy WHERE id_verktoy='$endre_id'";
+$resultat = $conn->query($sql);
+
+echo ""
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=ph, initial-scale=1.0">
-  <title>Document</title>
   <link rel="stylesheet" href="meny.css">
+  <title>Document</title>
 </head>
 <body>
 
+<form action="php">
+  
+</form>
 
-<?php
-include "meny.php";
 
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        <h1>endre data</h1>
+  
+        <form method="post">
+              
+              
+<p>
+                <label for="hylle">hylle:</label>
+                <input type="text" name="hylle" id="hylle">
+            </p>
+  
+  
+  
+              
+              
+<p>
+                <label for="kasse">kasse:</label>
+                <input type="text" name="kasse" id="kasse">
+            </p>
+  
+  
+  
+              
+              
+<p>
+                <label for="delenummer">delenumer:</label>
+                <input type="text" name="delenummer" id="delenummer">
+            </p>
+  
+  
+              
+              
+              
+<p>
+                <label for="id_kit">kit:</label>
+                <input type="text" name="id_kit" id="id_kit">
+            </p>
+  
 
-$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
 
-// Prepare statement
-$stmt = $conn->prepare($sql);
+ <p>
+                <label for="beskrivelse">beskrivelse:</label>
+                <input type="text" name="beskrivelse" id="beskrivelse">
+            </p> 
 
-// execute the query
-$stmt->execute();
 
-// melding om at vrekyet er opdatert
-echo $stmt->rowCount() . " dur har opdatert verktøyene i data basen";
-} catch(PDOException $e) {
-echo $sql . "<br>" . $e->getMessage();
-}
 
-$conn = null;
+<p>
+                <label for="verktoynummer">verktøynummer:</label>
+                <input type="text" name="verktoynummer" id="verktoynummer">
+            </p>
+          
+            
+                   
+<p>
+                <label for="status">status:</label>
+                <input type="text" name="status" id="status">
+            </p>
+  
+  
+              
+            <input name="submit" type="submit" value="Submit">
+        </form>
 
-?>
 
+
+</body>
 
 
 </body>
