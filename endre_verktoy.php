561c7fd13f97626229f37a0da7d3864290167df8
stig <92983848+stigamyr@users.noreply.github.com>
2022-03-24 08:20:36 +0100

    Update endre_verktoy.php

--- endre_verktoy.php
+++ endre_verktoy.php
@@ -4,12 +4,93 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=ph, initial-scale=1.0">
-  <link rel="stylesheet" href="meny.css">
-  <title>Document</title>
+    
+    <link rel="stylesheet" href="meny.css">
+
+    <style> 
+body {
+    background-image: url("Bakgrunn.jpg");
+}
+
+input[type=text] {
+  width:75%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  font-size: 15px;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+form {
+    margin-bottom: 50px;
+}
+
+h1 {
+    font-family: Arial, Helvetica, sans-serif;
+}
+.input {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+   
+}
+.tekst {
+    float: left;
+  width: 200px;
+  margin-top: 6px;
+  text-align: right;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.box {
+    float: left;
+  width: 75%;
+  margin-top: 6px; 
+}
+
+.kit_select {
+    width: 75%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    font-size: 15px;
+}
+.submit {
+    padding: 20px 20px;
+}
+
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 8px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  background-color: white;
+  color: black;
+  border: 2px solid #555555;
+}
+
+.button:hover {
+  background-color: #555555;
+  color: white;
+}
+</style>
+
 </head>
+
+
+
 <body>
   <?php
 include "meny.php";
@@ -55,36 +136,65 @@ while($rad = $resultat->fetch_assoc())  {
   $id_bruker = $rad["id_bruker"];
   $status = $rad["status"];
 
+  
+  echo"<center>";
+
 
   echo "id_verktøy";
+  echo "<div class='innpakning'>";
   echo " <input type='text' name='id_verktøy' id='id_verktøy' value='$id_verktoy' disabled>";
+  echo"hylle";
+
+
+
 
   echo"hylle";
+  echo "<div class='input'> ";
   echo"<input type='text' name='hylle' id='hylle' value='$hylle'>";
+  echo "</div>";
+
 
   echo"kasse";
+  echo "<div class='input'> ";
   echo" <input type='text' name='kasse' id='kasse'value='$kasse'>";
+  echo "</div>";
+
 
   echo"delenummer";
+  echo "<div class='input'> ";
   echo" <input type='text' name='delenummer' id='delenummer'value='$delenummer'>";
+  echo "</div>";
+
+
 
   echo"id_kit";
+  echo "<div class='input'> ";
   echo" <input type='text' name='id_kit' id='id_kit'value='$id_kit'>";
+  echo "</div>";
+
 
   echo"beskrivelse";
+  echo "<div class='input'> ";
   echo"  <input type='text' name='beskrivelse' id='beskrivelse'value='$beskrivelse'>";
+  echo "</div>";
+
+
+
 
   echo"verktoynummer";
+  echo "<div class='input'> ";
   echo"<input type='text' name='verktoynummer' id='verktoynummer' value='$verktoynummer'>";
-
+  echo "</div>";
   
 
 
 
-
+  echo "<div class='submit'>";
  echo " <input type='submit' name='endre' value='endre'> " ;
  echo "</form>";
+ echo "</div>"; 
 
+ echo"</center>";
 
 }
   
