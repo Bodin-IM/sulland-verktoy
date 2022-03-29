e33b72c5b142814865346c54de5addf246f00596
BBossYTeirik <eirikolufsen@hotmail.com>
2022-03-29 11:44:20 +0200

    masse css på verktoyvisning

--- sulland_verktoyvisning.php
+++ sulland_verktoyvisning.php
@@ -13,14 +13,36 @@
     <style>
 
 table {
-    width: 80%;
-    border: 3px solid black;
+    width: 100%;
+    height: 100%;
+    border: 1px solid black;
     background-color: #f2f2f2;
     border-collapse: collapse;
+    background: #0b2028;
+    color: #f2f2f2;
    
+}
+th, td {
+border-left: 1px solid black;
+border-right: 1px solid black;
+border-collapse: collapse;
+}
+tr {
+  height: 4vh;
+  
+}
+tr:nth-child(odd) {
+  background: #032530;
+}
+tr:nth-child(even) {
+  background: #0b2028;
+}
+tr:hover {
+  background: #0d313f;
 }
 .stil {
     width: 50%;
+    height: 50%;
     display: flex;
     justify-content: center;
     font-family: Drive,Helvetica,Arial,sans-serif;
@@ -28,12 +50,16 @@ table {
     left: 25%;
     
 }
+.rad:hover {
+  background: blue;
+}
+
 
 .logo {
     position: relative;
     height: 100px;
     width: 100%;
-    background-color: rgb(11, 30, 38);
+    background-color: #0b2028;
     margin: 0;
 }
 
@@ -86,57 +112,61 @@ table {
 
 <?php
 
-$tjener = "10.100.0.2";
-$brukernavn = "im2a";
-$passord = "Passord2";
-$database = "sulland_verktoy";
+  $tjener = "10.100.0.2";
+  $brukernavn = "im2a";
+  $passord = "Passord2";
+  $database = "sulland_verktoy";
 
-$kobling = new mysqli ($tjener, $brukernavn, $passord, $database);
+  $kobling = new mysqli ($tjener, $brukernavn, $passord, $database);
 
-$sql = "SELECT * FROM verktoy";
-$resultat = $kobling->query($sql);
-echo "<div class='logo'>";
-echo "</div>";
-echo "<div class='stil'>";
-echo "<table id='verktoytabell'>";
-echo "<tr>";
-    //echo "<th>id_verktøy</th>";
-    echo "<th>hylle</th>";
-    echo "<th>kasse</th>";
-    echo "<th>delenummer</th>";
-    echo "<th>id_kit</th>";
-    echo "<th>verktøynummer</th>";
-    echo "<th style='width:80%'>beskrivelse</th>";
-    echo "<th>id_bruker</th>";
-    echo "<th>status</th>";
-echo "</tr>";
+  $sql = "SELECT * FROM verktoy";
+  $resultat = $kobling->query($sql);
+  echo "<div class='logo'>";
+  echo "</div>";
+  echo "<div class='stil'>";
+    echo "<table id='verktoytabell'>";
+      echo "<tr>";
 
-while($rad = $resultat->fetch_assoc()) {
-    $id_verktøy = $rad["id_verktoy"];
-    $hylle = $rad["hylle"];
-    $kasse = $rad["kasse"];
-    $delenummer = $rad["delenummer"];
-    $id_kit = $rad["id_kit"];
-    $beskrivelse = $rad["beskrivelse"];
-    $verktøynummer = $rad["verktoynummer"];
-    $id_bruker = $rad["id_bruker"];
-    $status = $rad["status"];
+        //echo "<th>id_verktøy</th>";
+        echo "<th>hylle</th>";
+        echo "<th style='width:5%'>kasse</th>";
+        echo "<th>delenummer</th>";
+        echo "<th style='width:5%'>id_kit</th>";
+        echo "<th style='width:11%'>verktøynummer</th>";
+        echo "<th style='width:80%'>beskrivelse</th>";
+        echo "<th>id_bruker</th>";
+        echo "<th>status</th>";
 
-    echo "<tr>";
-        //echo "<td>$id_verktøy</td>";
-        echo "<td>$hylle</td>";
-        echo "<td>$kasse</td>";
-        echo "<td>$delenummer</td>";
-        echo "<td>$id_kit</td>";
-        echo "<td>$verktøynummer</td>";
-        echo "<td class='beskrivelse'>$beskrivelse</td>";
-        echo "<td>$id_bruker</td>";
-        echo "<td>$status</td>";
-    echo "</tr>";
-}
+      echo "</tr>";
 
-echo "</table>";
-echo "</div>"
+    while($rad = $resultat->fetch_assoc()) {
+      $id_verktøy = $rad["id_verktoy"];
+      $hylle = $rad["hylle"];
+      $kasse = $rad["kasse"];
+      $delenummer = $rad["delenummer"];
+      $id_kit = $rad["id_kit"];
+      $beskrivelse = $rad["beskrivelse"];
+      $verktøynummer = $rad["verktoynummer"];
+      $id_bruker = $rad["id_bruker"];
+      $status = $rad["status"];
+
+        echo "<tr>";
+          echo "<div class= 'rad'>";
+            //echo "<td>$id_verktøy</td>";
+            echo "<td>$hylle</td>";
+            echo "<td>$kasse</td>";
+            echo "<td>$delenummer</td>";
+            echo "<td>$id_kit</td>";
+            echo "<td>$verktøynummer</td>";
+            echo "<td class='beskrivelse'>$beskrivelse</td>";
+            echo "<td>$id_bruker</td>";
+            echo "<td>$status</td>";
+          echo "</div>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+  echo "</div>"
 ?>
 
 
