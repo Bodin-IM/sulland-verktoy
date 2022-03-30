8072926e65e19cdcc13793840c13fa5a0dd5f24a
otn2322 <otn.andreassen@hotmail.com>
2022-03-30 11:00:07 +0200

    suiii
    suiii

--- sulland_verktoyvisning.php
+++ sulland_verktoyvisning.php
@@ -10,6 +10,11 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+
+
+
+
+
     <style>
 
 table {
@@ -102,6 +107,8 @@ tr:hover {
 <body>
 
 
+
+
 <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
 
 <?php
@@ -163,6 +170,9 @@ tr:hover {
 ?>
 
 
+
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
 <script>
@@ -196,8 +206,6 @@ tr:hover {
   }
 }
 </script>
-
-
 
 
 </body>
