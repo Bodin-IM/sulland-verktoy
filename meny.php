c90cba30d4e9f2dd627ce0a6376245489ffec8ae
NicolaiMorfIM2A <92984039+NicolaiMorfIM2A@users.noreply.github.com>
2022-04-08 13:06:34 +0200

    fjernet deploy.php og menyvalg for den

--- meny.php
+++ meny.php
@@ -4,7 +4,6 @@
     <li><a href="/opprett.php">Opprett</a></li>
     <li><a href="/slett.php">Slette Verktøy</a></li>
     <li><a href="/endre_alle.php">Endre Verktøy</a></li>
-    <li><a href="/deploy.php">Deploy</a></li>
     <li><a href="/user_admin.php">User Admin</a></li>
     <li><a href="/brukere.php">brukere</a></li>
 </ul> 
