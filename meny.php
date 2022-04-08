c8e138093db23c33f31b78df993a7e36edf6c933
zakariaadem <zakaria1adam1@gmail.com>
2022-04-08 13:39:49 +0200

    fixed starts page

--- meny.php
+++ meny.php
@@ -1,11 +1,9 @@
 <ul>
-    <li><a class="active" href="/index.php">Home</a></li>
     <li><a href="/sulland.php">Verktøyvisning</a></li>
     <li><a href="/opprett.php">Opprett</a></li>
     <li><a href="/slett.php">Slette Verktøy</a></li>
     <li><a href="/endre_alle.php">Endre Verktøy</a></li>
     <li><a href="/deploy.php">Deploy</a></li>
-    <li><a href="/user_admin.php">User Admin</a></li>
-    <li><a href="/sok.php">søkefunksjon</a></li>
-    <li><a href="/brukere.php">brukere</a></li>
+    <li><a href="/user_admin.php">Ny Bruker</a></li>
+    <li id=''><a href="/slett.php">Admin</a></li>
 </ul> 
