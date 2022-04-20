b01c00f3f11ea85b79a10a8a9e383671c72d58f7
zakariaadem <zakaria1adam1@gmail.com>
2022-04-20 10:38:37 +0200

    Update meny.php

--- meny.php
+++ meny.php
@@ -1,9 +1,4 @@
 <ul>
     <li><a href="/sulland.php">Verktøyvisning</a></li>
-    <li><a href="/opprett.php">Opprett</a></li>
-    <li><a href="/slett.php">Slette Verktøy</a></li>
-    <li><a href="/endre_alle.php">Endre Verktøy</a></li>
-    <li><a href="/deploy.php">Deploy</a></li>
-    <li><a href="/user_admin.php">Ny Bruker</a></li>
     <li id=''><a href="/slett.php">Admin</a></li>
 </ul> 
