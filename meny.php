7e5a9582b3ceedfb01f837e7f4cbd5e1a383e45d
zakariaadem <zakaria1adam1@gmail.com>
2022-05-04 10:31:02 +0200

    fixed admin

--- meny.php
+++ meny.php
@@ -1,6 +1,6 @@
 <div id="meny">
     <div><a href="/opprett.php">Opprett</a></div>
-    <div><a href="/slett.php">Slette / Endre Verktøy</a></div>
+    <div><a href="/admin.php">Slette / Endre Verktøy</a></div>
     <div><a href="/user_admin.php">Ny Bruker</a></div>
     <div id="log_ut"><a href="/index.php">Log ut</a></div>
 </div>
