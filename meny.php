12245b3e1fc6038b258bd304d775726bb2e3d403
NikkoThatGuy <NikkoThatGamerGuy@mail.com>
2022-05-16 10:47:20 +0200

    utlån tilgang
    lå til en knap på admin sånn at mn kan komme inn på alle utlån

--- meny.php
+++ meny.php
@@ -2,5 +2,6 @@
     <div><a href="/opprett.php">Opprett</a></div>
     <div><a href="/admin.php">Slette / Endre Verktøy</a></div>
     <div><a href="/user_admin.php">Ny Bruker</a></div>
+    <div><a href="/alle_utlan.php">Alle Utlån</a></div>
     <div id="log_ut"><a href="/index.php">Log ut</a></div>
 </div>
