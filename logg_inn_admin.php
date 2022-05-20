b5c9f0947531d352a18e5e25501750d9852a0c5c
zakariaadem <92983962+zakariaadem@users.noreply.github.com>
2022-05-20 13:56:32 +0200

    fixed feil melding

--- logg_inn_admin.php
+++ logg_inn_admin.php
@@ -11,7 +11,8 @@
             die();
         }
         else{
-            echo "feil";
+            $_SESSION['feilmelding'] = "Du skrev feil passord";
+            header('Location: logg_inn_admin.php');
             die();
         }
     }
@@ -27,11 +28,19 @@
 </head>
 <body>
     <div>
-        <h1>Logg Inn Som Admin</h1>
+        <h1>logg inn som admin</h1>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
             <input type="password" name="passord" placeholder="Passord" required>
             <button type='submit' name='logg_inn'>Logg inn</button>
         </form>
+        
+        <?php    
+            if (isset($_SESSION['feilmelding'])){ 
+            echo $_SESSION['feilmelding']; }
+            session_unset();
+            session_destroy();
+        ?>
+
     </div>
 </body>
 </html>
--- meny.php
+++ meny.php
@@ -1,8 +1,8 @@
 <div id="meny">
+    <div><a href="/admin.php">Home</a></div>
     <div><a href="/opprett.php">Opprett</a></div>
-    <div><a href="/admin.php">Slette / Endre Verktøy</a></div>
     <div><a href="/user_admin.php">Ny Bruker</a></div>
     <div><a href="/alle_utlan.php">Alle Utlån</a></div>
-    <div id="log_ut"><a href="/log_ut.php">Log ut</a></div>
+    <div id="log_ut"><a href="/log_ut.php">Logg ut</a></div>
 </div>
 
