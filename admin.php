ad6b881ac9a53e1916c67cf7e59369cdf2eece2b
zakariaadem <zakaria1adam1@gmail.com>
2022-06-08 14:59:13 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sulland - verktøy</title>
     <link rel="stylesheet" href="css/admin_side.css">
     <link rel="stylesheet" href="css/admin_meny.css">
     <!-- En link for å hente font fra google  --> 
